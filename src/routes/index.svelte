58d876dfade3421e4f3c8db35451ee814f6ed67a
aidan-starke <starke.aidan@gmail.com>
2022-06-06 21:13:24 +1200

    sveltedex

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,31 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+import type { Poke, Pokemon } from "@/types";
+
+	import { pokemon } from "@/stores/pokemart";
+	import PokeCard from "@/components/pokeCard.svelte";
+
+	let searchTerm = "";
+	let filteredPokemon = [] as Pokemon[];
+
+	$: {
+		if (searchTerm) {
+			filteredPokemon = $pokemon.filter((poke: Poke) => poke.name.toLowerCase().includes(searchTerm.toLowerCase()))
+		} else {
+			filteredPokemon = [...$pokemon]
+		}
+	}
+</script>
+
+<svelte:head>
+	<title>SvelteDex | Home</title>
+</svelte:head>
+
+<h1 class="text-4xl text-center my-8">SvelteDex</h1>
+
+<input type="text" placeholder="Search Pokemon" class="w-full rounded-md text-lg p-4 border-2 border-gray-200" bind:value={searchTerm} />
+
+<div class="grid gap-4 md:grid-cols-2 grid-cols-1 py-4">
+	{#each filteredPokemon as poke}
+		<PokeCard {poke} />
+	{/each}
+</div>
--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,25 @@
-import adapter from '@sveltejs/adapter-auto';
-import preprocess from 'svelte-preprocess';
+import adapter from "@sveltejs/adapter-auto";
+import preprocess from "svelte-preprocess";
+import path from "path";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
-	preprocess: preprocess(),
+	preprocess: [
+		preprocess({
+			postcss: true,
+		}),
+	],
 
 	kit: {
-		adapter: adapter()
-	}
+		adapter: adapter(),
+		vite: {
+			resolve: {
+				alias: { "@": path.resolve("./src") },
+			},
+		},
+	},
 };
 
 export default config;
